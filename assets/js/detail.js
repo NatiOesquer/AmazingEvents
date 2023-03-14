98e7a84c3622b135cc5c4202f69605eef737fd43
NatiOesquer <nati.oesquer@hotmail.com>
2023-03-14 09:56:21 -0300

    corrigiendo error de card de details task3

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -7,23 +7,19 @@ const eventos = data.events.find((event) => event._id == eventDetail);
 
 let card = document.getElementById('nuevaDetail');
 const cardDetalle = document.querySelector('.detalleCard');
-cardDetalle.innerHTML = `<div class="estilotarjeta">
-<div class="card mb-3" style="max-width: 66%;">
-<div class="row g-0">
-<div id="image" class="col-md-4">
-<img src="${eventos.image}" class="img-fluid rounded-start">
-</div>
-<div id="texto" class="col-md-12">
-<div class="card-body">
-  <h5 class="card-individual_titulo " style>${eventos.name}</h5>
-  <p class="texto-arial centrado subrayado">${eventos.description}</p>
-  <p class="texto-negrita texto-alado">Fecha:&nbsp;</p><p class="texto-alado"> ${eventos.date}</p><br />
-  <p class="texto-negrita texto-alado">Categoria:&nbsp;</p><p class="texto-alado">${eventos.category}</p><br />
-  <p class="texto-negrita texto-alado">Sala:&nbsp;</p><p class="texto-alado">${eventos.place}</p><br />
-  <p class="texto-negrita texto-alado">Capacidad:&nbsp;</p><p class="texto-alado">${eventos.capacity}</p> <br> <p class="texto-negrita texto-alado">Asistencia:&nbsp;</p><p class="texto-alado">${eventos.assistance}</p><br />
-  <p class="texto-negrita texto-alado">Precio:&nbsp;$&nbsp;</p><p class="texto-alado " style=" font-size: 1em;">${eventos.price}</p>
-</div>
-</div>
-</div>
+cardDetalle.innerHTML = `<div class=" d-flex justify-content-center align-items-center p-4 card mb-3" style="max-width: 800px; height: 400px;">
+<div class="row align-items-center justify-content-center " style="width:100% ; height:100% ;">
+  <div class="col-md-4" style="width:50% ; height:80% ;">
+    <img src="${eventos.image}" class="img-fluid rounded-start" alt="..." style="width:100% ; height:100% ;">
+  </div>
+  <div class="col-md-8" style="width:50% ; height:80% ;">
+    <div class="card-body">
+      <h5 class="card-title">${eventos.name}</h5>
+      <p class="card-text">${eventos.description}</p>
+      <p class="card-text">Date: ${eventos.date} /Category: ${eventos.category} /Place: ${eventos.place}</p>
+      <p class="card-text">Capacity: ${eventos.capacity} /Assistance: ${eventos.assistance}</p>
+      <p class="card-text">Price: $${eventos.price}</p>
+    </div>
+  </div>
 </div>
 </div>`;
